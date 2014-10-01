d8d05bc611b88231daf8906bf896e41a1cb13b11
Lubomir Hadzidev <lubomir.hadziev@gmail.com>
2014-10-01 12:07:47 +0300

    add new homework
    conditional statements

---
+++ Conditional-Statements-Homework/Conditional-Statements-Homework/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            DateTime inputDate = DateTime.Parse(Console.ReadLine());
+
+            if (inputDate.Hour >= 13 || inputDate.Hour < 3)
+            {
+                Console.WriteLine("beer time");
+            }
+            else
+            {
+                Console.WriteLine("non-beer time");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
