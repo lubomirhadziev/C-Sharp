a281a1af63ec092b18fedabf54807bb1b372a602
Lubomir Hadzidev <lubomir.hadziev@gmail.com>
2014-10-03 12:17:46 +0300

    add problem 9 matrix of numbers

---
+++ 6. Loops-Homework/Problem 9. Matrix of Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            byte n = byte.Parse(Console.ReadLine());
+
+            for (byte row = 1; row <= n; row++)
+            {
+                for (byte col = row; col < (n + row); col++)
+                {
+                    Console.Write("{0,-3}", col);
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine();
+        }
+    }
+}
