2e200637f42f2cc8e15ac0f1201ce3a62884fd8b
Lubomir Hadzidev <lubomir.hadziev@gmail.com>
2014-10-09 12:19:52 +0300

    solution for Decimal to Binary Number

---
+++ 6. Loops-Homework/Problem 14. Decimal to Binary Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            double number = double.Parse(Console.ReadLine());
+            string binary = "";
+            double divisible = number;
+
+            while (divisible >= 1)
+            {
+                binary += divisible % 2;
+                divisible = (int)(divisible / 2);
+
+                //for debug only --> Console.WriteLine("{0}:2={1} | {2}", oldDivisible, divisible, oldDivisible%2);
+            }
+
+            //convert to char array and reverse the chars
+            char[] charArray = binary.ToCharArray();
+            Array.Reverse(charArray);
+
+            Console.WriteLine(new string(charArray));
+            Console.WriteLine();
+        }
+    }
+}
