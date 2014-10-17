4881f0a69a812a7cf9ea788d0964b5a0f1a7f5db
Lubomir Hadzidev <lubomir.hadziev@gmail.com>
2014-10-17 23:44:39 +0300

    solution for some of problems in advanced topic

---
+++ CS Advanced Topics/CS Advanced Topics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while(true)
+        {
+            int fibNumber = Fib(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine(fibNumber);
+            Console.WriteLine();
+        }
+    }
+
+    static int Fib(int n)
+    {
+        n += 2;
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i > 1)
+            {
+                numbers[i] = (numbers[i - 1] + numbers[i - 2]);
+            }
+            else
+            {
+                numbers[i] = i;
+            }
+        }
+
+        return numbers[n-1];
+    }
+}
