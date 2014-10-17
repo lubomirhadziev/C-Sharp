fc1ed71294f94e406caacbf603889ee5a1a7adb4
Lubomir Hadzidev <lubomir.hadziev@gmail.com>
2014-10-17 23:45:11 +0300

    some changes

---
+++ CS Advanced Topics/Problem 2. Prime Checker/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while(true)
+        {
+            bool isPrimeNumber = isPrime(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("{0}", isPrimeNumber);
+            Console.WriteLine();
+        }
+    }
+
+
+    static bool isPrime(double n)
+    {
+        n = Math.Abs(n);
+
+        double sqrt = Math.Sqrt(n);
+        bool isPrime = true;
+
+        if (n < 2)
+        {
+            isPrime = false;
+        }
+        else
+        {
+            for (int i = 2; i <= sqrt; i++)
+            {
+                if (n % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        return isPrime;
+    }
+
+}
---
+++ CS Advanced Topics/Problem 3. Primes in Given Range/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            int startNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+
+            var list = FindPrimesInRange(startNum, endNum);
+
+            Console.WriteLine("{0}", string.Join(", ", Array.ConvertAll(list.ToArray(), i => i.ToString())));
+            Console.WriteLine();
+        }
+    }
+
+    static List<int> FindPrimesInRange(int startNum, int endNum)
+    {
+        var primeNumbers = new List<int>();
+
+        for (int i = startNum; i <= endNum; i++)
+        {
+            if (isPrime(i))
+            {
+                primeNumbers.Add(i);
+            }
+        }
+
+        return primeNumbers;
+    }
+
+    static bool isPrime(int n)
+    {
+        n = Math.Abs(n);
+
+        int sqrt = (int)Math.Sqrt((double)n);
+        bool isPrime = true;
+
+        if (n < 2)
+        {
+            isPrime = false;
+        }
+        else
+        {
+            for (int i = 2; i <= sqrt; i++)
+            {
+                if (n % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        return isPrime;
+    }
+}
